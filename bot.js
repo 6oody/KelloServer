0e8f89a27b8868f977d91b9507ff2ff64c0ba23a
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2018-08-20 23:45:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2411,4 +2411,43 @@ omar.reply("`تم حذف الرومات بنجاح`")
 		}
 	
 	});
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  var prefix = "K";
+  let messageArray = message.content.split (" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+
+
+
+
+
+  if(cmd === `${prefix}report`){
+
+
+
+  let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!rUser) return message.channel.send("لم اجد العضو");
+  let reason = args.join(" ").slice(22);
+
+  let reportEmbed = new Discord.RichEmbed()
+  .setDescription("Reports")
+  .setColor("#15f153")
+  .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+  .addField("Reported by", `${message.author} with ID: ${message.author.id}`)
+  .addField("Channel", message.channel)
+  .addField("Time", message.createdAt)
+  .addField("Reason", reason);
+
+ let reportschannel = message.guild.channels.find("name", "reports");
+ if(!reportschannel) return message.channel.send("لم اجد الروم حقت الريبورتات يخوي لول");
+
+
+ message.delete().catch(O_o=> {});
+ reportschannel.send(reportEmbed);
+}
+});
 client.login(process.env.BOT_TOKEN);
