7608415624dcffde6e8fe05c04d50de3e5732e4d
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2018-09-15 22:56:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = 'D'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`.Black Night`,"http://twitch.tv/S-F")
+client.user.setGame(`Daaaark,,,,,,!`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
